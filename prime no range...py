3efdacb4e120812cd23e88b510f67c083a02ef72
shivamrai2298 <shivamrai2298@gmail.com>
2016-10-18 23:18:54 +0530

    Add files via upload

---
+++ prime no range...py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter lower range: "))
+num2 = int(input("Enter upper range: "))
+for num in range(num1,num2 + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
